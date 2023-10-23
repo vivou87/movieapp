cf02c7b56d56a3b1a79d39801727651a42815e2d
vivou87 <olfapro.1987@gmail.com>
2023-10-23 12:07:24 +0100

    second commit

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -52,11 +52,11 @@ function SingleMovie() {
         </Container>
       </Navbar>
       <div className="singleMovie">
-        <h1> {movie.title} </h1>
+        <h1> {movie?.title} </h1>
         <iframe
           width="560"
           height="315"
-          src={movie.trailer}
+          src={movie?.trailer}
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
